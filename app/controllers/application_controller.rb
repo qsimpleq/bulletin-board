0b1b58f9f3c0a8d06c30ed1c13999bc7acaa3b41
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-07-16 14:38:18 +0500

    Move common helpers to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,28 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  helper_method %i[current_user back_path user_signed_in?]
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+    pp @current_user
+    @current_user
+  end
+
+  def back_path(**params)
+    params[:action_name] ||= action_name
+    params[:controller_name] ||= controller_name
+
+    if %w[new show].include?(params[:action_name])
+      url_for(controller: params[:controller_name], action: :index, only_path: true)
+    elsif %w[edit update].include?(params[:action_name])
+      url_for(controller: params[:controller_name], action: :show, only_path: true)
+    else
+      params[:default_path] || request.referer
+    end
+  end
+
+  def user_signed_in?
+    current_user ? true : false
+  end
 end
--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -2,27 +2,5 @@
 
 module Web
   class ApplicationController < ApplicationController
-    helper_method %i[current_user back_path user_signed_in?]
-
-    def current_user
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
-
-    def back_path(**params)
-      params[:action_name] ||= action_name
-      params[:controller_name] ||= controller_name
-
-      if %w[new show].include?(params[:action_name])
-        url_for(controller: params[:controller_name], action: :index, only_path: true)
-      elsif %w[edit update].include?(params[:action_name])
-        url_for(controller: params[:controller_name], action: :show, only_path: true)
-      else
-        params[:default_path] || request.referer
-      end
-    end
-
-    def user_signed_in?
-      current_user ? true : false
-    end
   end
 end
