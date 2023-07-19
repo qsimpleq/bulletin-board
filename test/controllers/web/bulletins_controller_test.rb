8af438e51a5bdea3fa84fee2b6b2e1b53b2cd1f4
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-07-19 12:33:58 +0500

    Test fixes, add fixture images

--- test/controllers/web/bulletins_controller_test.rb
+++ test/controllers/web/bulletins_controller_test.rb
@@ -5,7 +5,9 @@ require 'test_helper'
 module Web
   class BulletinsControllerTest < ActionDispatch::IntegrationTest
     setup do
+      @user_one = users(:one)
       @bulletin = bulletins(:one)
+      sign_in(@user_one)
     end
 
     test 'should get index' do
@@ -17,14 +19,20 @@ module Web
     test 'should get new' do
       get new_bulletin_url
 
-      assert_redirected_to root_url
+      assert_response :success
     end
 
     test 'should create bulletin' do
       assert_difference('Bulletin.count') do
-        post bulletins_url,
-             params: { bulletin: { description: @bulletin.description + '1',
-                                   title: @bulletin.title + '1' } }
+        image_path = Rails.root.join('test/fixtures/files/vehicle_car.jpg')
+        image_file = fixture_file_upload(image_path, 'image/jpeg')
+
+        post bulletins_url, params: {
+          bulletin: { category_id: @bulletin.category_id,
+                      description: @bulletin.description,
+                      title: @bulletin.title,
+                      image: image_file }
+        }
       end
 
       assert_redirected_to bulletins_url
