d7c27ecd77fad32a788ba40c5e259e86c312dda7
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-07-30 18:36:09 +0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@
 
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  post 'auth/:provider', to: 'web/auth#request', as: :auth_request
-  get 'auth/:provider/callback', to: 'web/auth#callback', as: :callback_auth
-  delete 'auth/logout', to: 'web/auth#destroy'
-
   scope module: :web do
+    post 'auth/:provider', to: 'auth#request', as: :auth_request
+    get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
+    delete 'auth/logout', to: 'auth#destroy'
+
     root 'bulletins#index'
 
     get 'admin', to: 'admin/bulletins#under_moderation'
